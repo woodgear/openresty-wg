2d05bf1ff080bdf4a7deac1545b38b8c48314dd6
congwu <congwu@alauda.io>
2021-11-08 23:03:18 +0800

    uppgrade to 1.19.9.1

--- util/build-win32.sh
+++ util/build-win32.sh
@@ -3,9 +3,9 @@
 PCRE=pcre-8.44
 ZLIB=zlib-1.2.11
 OPENSSL=openssl-1.1.1k
-JOBS=8
+JOBS=12
 
-# wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+# wget https://www.openssl.org/source/openssl-1.1.1k.tar.gz
 # wget http://zlib.net/zlib-1.2.11.tar.gz
 # wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz
 
@@ -29,7 +29,6 @@ cd ../../..
 
 ./configure \
     --with-cc=gcc \
-    --with-ipv6 \
     --prefix= \
     --with-cc-opt='-DFD_SETSIZE=1024' \
     --sbin-path=nginx.exe \
